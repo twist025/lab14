734bb6da3de15e90a6cba0c627168e9187875203
twist025 <twist025@mail.ru>
2017-06-29 11:43:42 +0300

    refactoring

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-[![Build Status](https://travis-ci.org/twist025/lab11.svg?branch=master)](https://travis-ci.org/twist025/lab11)
+[![Build Status](https://travis-ci.org/twist025/lab12.svg?branch=master)](https://travis-ci.org/twist025/lab12)
 the demo application redirects data from stdin to a file **log.txt** using a package **print**.
--- sources/demo.cpp
+++ sources/demo.cpp
@@ -1,9 +1,11 @@
 #include <print.hpp>
+#include <cstdlib>
 
 int main(int argc, char** argv) {
         std::string text;
+std::string log_path = std::getenv("LOG_PATH");
+	std::ofstream out(log_path, std::ios_base::app);
         while(std::cin >> text) {
-                std::ofstream out("log.txt", std::ios_base::app);
                 print(text, out);
                 out << std::endl;
         }
